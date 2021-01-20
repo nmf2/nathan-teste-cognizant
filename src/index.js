690852682d3c0dfe4ae90455fee68a69751f7a6a
Nathan <nmf2@cin.ufpe.br>
2021-01-20 15:24:52 -0300

    refactor: apply eslint rules

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-import express from "express";
-import routes from "./routes";
-import * as bodyParser from "body-parser";
-import { middleware as ValidatorOpenApi } from "express-openapi-validator";
-import path from "path";
+import express from 'express'
+import routes from './routes'
+import * as bodyParser from 'body-parser'
+import { middleware as ValidatorOpenApi } from 'express-openapi-validator'
+import path from 'path'
 // import swaggerUi from "swagger-ui-express";
 import fs from 'fs'
-import YAML from "yamljs";
+import YAML from 'yamljs'
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-
-app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || "800kb" }));
+app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '800kb' }))
 app.use(
   bodyParser.urlencoded({
     extended: true,
-    limit: process.env.REQUEST_LIMIT || "800kb",
+    limit: process.env.REQUEST_LIMIT || '800kb'
   })
-);
+)
 
-const apiSpec = path.join(__dirname, "./api.yaml");
-const swaggerDocument = YAML.load(apiSpec);
+const apiSpec = path.join(__dirname, './api.yaml')
+const swaggerDocument = YAML.load(apiSpec)
 
 // Allows to open the API spec and test page at /api-explorer
-app.use(express.static(`public`))
+app.use(express.static('public'))
 fs.writeFileSync(apiSpec, JSON.stringify(swaggerDocument))
 app.use(process.env.OPENAPI_SPEC || '/spec', express.static(apiSpec))
 
@@ -31,19 +30,19 @@ app.use(process.env.OPENAPI_SPEC || '/spec', express.static(apiSpec))
 app.use(
   ValidatorOpenApi({
     apiSpec,
-    validateRequests: true,
+    validateRequests: true
   }),
   (err, req, res, next) => {
     res.status(err.status || 500).json({
       message: err.message,
-      errors: err.errors,
-    });
+      errors: err.errors
+    })
   }
-);
+)
 
-routes(app);
+routes(app)
 
 app.listen(port, () => {
-  console.log(`Server up and runnig on port ${port}`);
-  console.log(`Documentation at http://localhost:${port}/api-explorer`);
-});
+  console.log(`Server up and runnig on port ${port}`)
+  console.log(`Documentation at http://localhost:${port}/api-explorer`)
+})
--- src/resources/raceStats/service.js
+++ src/resources/raceStats/service.js
@@ -1,37 +1,42 @@
-import UserInputError from "@/errors/userInputError";
-import csvParser from "neat-csv";
-import fsPromises from "fs/promises";
-import path from "path";
-import { stat } from "fs";
+import UserInputError from '@/errors/userInputError'
+import csvParser from 'neat-csv'
+import fsPromises from 'fs/promises'
+import path from 'path'
 
 export default class Service {
+  #defaultCsv;
   /**
    * Loads the default CSV file
    */
-  async #loadDefaultCsv() {
+  async #loadDefaultCsv () {
     // Could also set file contents directly to a variable depending on performance needs
     try {
-      return await fsPromises.readFile(path.join(__dirname, "defaultLog.csv"));
+      if (!this.#defaultCsv) {
+        this.#defaultCsv = await fsPromises.readFile(
+          path.join(__dirname, 'defaultLog.csv')
+        )
+      }
+      return this.#defaultCsv
     } catch (e) {
-      console.error(e);
-      throw new Error("Error loading default log file");
+      console.error(e)
+      throw new Error('Error loading default log file')
     }
   }
 
   /**
    * Parses the given log (parses the default log if no arguments are passed)
-   * @param {string} csvText 
+   * @param {string} csvText
    */
-  async #loadAndValidateCsv(csvText) {
+  async #loadAndValidateCsv (csvText) {
     if (!csvText) {
-      csvText = await this.#loadDefaultCsv();
+      csvText = await this.#loadDefaultCsv()
     }
 
     try {
       // For now assume format will always be correct
-      return await csvParser(csvText, { separator: ";" });
+      return await csvParser(csvText, { separator: ';' })
     } catch (err) {
-      throw new UserInputError("Invalid CSV");
+      throw new UserInputError('Invalid CSV')
     }
   }
 
@@ -54,7 +59,7 @@ export default class Service {
    * @return {HeroStats}
    */
   #getDefaultStats (hero) {
-    const [code, name] = hero.split("–");
+    const [code, name] = hero.split('–')
     return {
       code: parseInt(code),
       name,
@@ -65,77 +70,77 @@ export default class Service {
       totalTimeMs: 0,
       totalTimeText: null,
       overallAverageSpeed: 0
-    };
-  };
+    }
+  }
 
   /**
    * Returns the race stats from the CSV text
-   * @param {string} csvText 
+   * @param {string} csvText
    */
-  async getStats(csvText) {
-    const csv = await this.#loadAndValidateCsv(csvText);
-    
+  async getStats (csvText) {
+    const csv = await this.#loadAndValidateCsv(csvText)
+
     /**
      * @type {Map<string, HeroStats>}
      */
-    const statsMap = new Map();
+    const statsMap = new Map()
 
     for (const lap of csv) {
-      const hero = lap["Super-Heroi"];
+      const hero = lap['Super-Heroi']
       const speed = parseFloat(
-        lap["Velocidade média da volta"].replace(",", ".")
-      );
-      const timeText = lap["Tempo Volta"];
-      const lapNumber = parseFloat(lap["No Volta"]);
+        lap['Velocidade média da volta'].replace(',', '.')
+      )
+      const timeText = lap['Tempo Volta']
+      const lapNumber = parseFloat(lap['No Volta'])
 
       if (!statsMap.has(hero)) {
-        statsMap.set(hero, this.#getDefaultStats(hero));
+        statsMap.set(hero, this.#getDefaultStats(hero))
       }
 
       // Split text on anything that is not a number at most 3 times
-      const [minutes, seconds, ms] = timeText.split(/[^\d]/g, 3);
+      const [minutes, seconds, ms] = timeText.split(/[^\d]/g, 3)
 
       // Save the total time in millisecods
       const totalTimeMs =
-        parseInt(minutes) * 60 * 1000 + parseInt(seconds) * 1000 + parseInt(ms);
+        parseInt(minutes) * 60 * 1000 + parseInt(seconds) * 1000 + parseInt(ms)
 
-      const currentHeroStats = statsMap.get(hero);
+      const currentHeroStats = statsMap.get(hero)
 
       // Assuming the speed unit is distance per miliseconds
       if (currentHeroStats.bestLapSpeed < speed) {
-        currentHeroStats.bestLapSpeed = speed;
-        currentHeroStats.bestLap = lapNumber;
+        currentHeroStats.bestLapSpeed = speed
+        currentHeroStats.bestLap = lapNumber
       }
 
-      currentHeroStats.position += speed * totalTimeMs;
-      currentHeroStats.totalTimeMs += totalTimeMs;
-      currentHeroStats.totalLaps++;
+      currentHeroStats.position += speed * totalTimeMs
+      currentHeroStats.totalTimeMs += totalTimeMs
+      currentHeroStats.totalLaps++
     }
 
-    const stats = [];
+    const stats = []
 
     /**
      * Calculate total time and overallAverageSpeed of a hero.
      */
     for (const stat of statsMap.values()) {
-      const totalTimeMs = stat.totalTimeMs;
-      const dateDuration = new Date(stat.totalTimeMs);
-      const minutes = `${dateDuration.getMinutes()}`.padStart(2, "0");
-      const seconds = `${dateDuration.getSeconds()}`.padStart(2, "0");
-      const ms = `${dateDuration.getMilliseconds()}`.padStart(3, "0");
-      stat.totalTimeText = `${minutes}:${seconds}.${ms}`;
-      stat.overallAverageSpeed = stat.position / totalTimeMs;
-      stats.push(stat);
+      const totalTimeMs = stat.totalTimeMs
+      const dateDuration = new Date(stat.totalTimeMs)
+      const minutes = `${dateDuration.getMinutes()}`.padStart(2, '0')
+      const seconds = `${dateDuration.getSeconds()}`.padStart(2, '0')
+      const ms = `${dateDuration.getMilliseconds()}`.padStart(3, '0')
+      stat.totalTimeText = `${minutes}:${seconds}.${ms}`
+      stat.overallAverageSpeed = stat.position / totalTimeMs
+      stats.push(stat)
     }
 
     // Sort stats by best bestLapSpeed
-    stats.sort((stat1, stat2) => stat2.bestLapSpeed - stat1.bestLapSpeed);
+    stats.sort((stat1, stat2) => stat2.bestLapSpeed - stat1.bestLapSpeed)
 
     const result = {
       herosStas: stats,
       bestOverallLapSpeed: stats[0].bestLapSpeed
-    };
+    }
 
-    return result;
+    return result
   }
 }
