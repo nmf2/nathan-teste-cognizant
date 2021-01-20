a29af320be713a6b850f40db857e5ac2a66773e4
Nathan <nmf2@cin.ufpe.br>
2021-01-19 23:43:35 -0300

    feat(image): bulk value count endpoint

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-
+import imageRouter from './resources/image/router'
 /**
  * @param {import("express").Application} app
  */
 export default function router(app) {
-  
+  app.use('/image', imageRouter)
 }
